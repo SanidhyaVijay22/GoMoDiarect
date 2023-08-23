82469cfe556ae677b09b43217c67f32e42331f26
Sanidhya Vijay <svijay_be20@thapar.edu>
2023-08-23 12:36:51 +0530

    Profile Page

--- lib/screens/reports_screen.dart
+++ lib/screens/reports_screen.dart
@@ -6,45 +6,53 @@ class Reports extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Past Diagnosis History"),
-          backgroundColor: Colors.deepPurpleAccent
-      ),
-
-      body: Column(
-          children: <Widget>[
-            Card (
-        margin: EdgeInsets.all(10),
-        color: Colors.purple[200],
-        shadowColor: Colors.blueGrey,
-        elevation: 10,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Image.asset('images/iris1.jpeg'),// leading: Icon (
-              //     Icons.album,
-              //     color: Colors.cyan,
-              //     size: 45
-              title: Text(
-                "Level 0",
-                style: TextStyle(fontSize: 20),
-              ),
-              subtitle: Text('22/7/2001'),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("R E P O R T S"),
+          backgroundColor: Colors.deepPurpleAccent,
+          automaticallyImplyLeading: false,
         ),
-      ),
-            Card (
-              margin: EdgeInsets.all(10),
-              color: Colors.blue[200],
-              shadowColor: Colors.blueGrey,
-              elevation: 10,
+        body: Column(children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.deepPurple[200],
+            shadowColor: Colors.blueGrey,
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 30.0, top: 30.0, bottom: 30.0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   ListTile(
-                    leading: Image.asset('images/iris1.jpeg'),// leading: Icon (
+                    leading:
+                        Image.asset('images/iris1.jpeg'), // leading: Icon (
+                    //     Icons.album,
+                    //     color: Colors.cyan,
+                    //     size: 45
+                    title: Text(
+                      "Level 0",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    subtitle: Text('22/7/2001'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.deepPurple[200],
+            shadowColor: Colors.blueGrey,
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 30.0, top: 30.0, bottom: 30.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading:
+                        Image.asset('images/iris1.jpeg'), // leading: Icon (
                     //     Icons.album,
                     //     color: Colors.cyan,
                     //     size: 45
@@ -57,17 +65,21 @@ class Reports extends StatelessWidget {
                 ],
               ),
             ),
-
-            Card (
-              margin: EdgeInsets.all(10),
-              color: Colors.purple[200],
-              shadowColor: Colors.blueGrey,
-              elevation: 10,
+          ),
+          Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.deepPurple[200],
+            shadowColor: Colors.blueGrey,
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 30.0, top: 30.0, bottom: 30.0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   ListTile(
-                    leading: Image.asset('images/iris1.jpeg'),// leading: Icon (
+                    leading:
+                        Image.asset('images/iris1.jpeg'), // leading: Icon (
                     //     Icons.album,
                     //     color: Colors.cyan,
                     //     size: 45
@@ -80,17 +92,21 @@ class Reports extends StatelessWidget {
                 ],
               ),
             ),
-
-            Card (
-              margin: EdgeInsets.all(10),
-              color: Colors.blue[200],
-              shadowColor: Colors.blueGrey,
-              elevation: 10,
+          ),
+          Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.deepPurple[200],
+            shadowColor: Colors.blueGrey,
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 30.0, top: 30.0, bottom: 30.0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   ListTile(
-                    leading: Image.asset('images/iris1.jpeg'),// leading: Icon (
+                    leading:
+                        Image.asset('images/iris1.jpeg'), // leading: Icon (
                     //     Icons.album,
                     //     color: Colors.cyan,
                     //     size: 45
@@ -103,9 +119,7 @@ class Reports extends StatelessWidget {
                 ],
               ),
             ),
-        ]
-
-      )
-    );
+          ),
+        ]));
   }
 }
